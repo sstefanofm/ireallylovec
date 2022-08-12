6fb303b1a704ab25f90eb9e9723099cb6acb5bef
stf <sstefanofm@gmail.com>
2022-08-12 01:54:51 -0300

    with for loop but grimmer;

--- 1/FCtable_for.c
+++ 1/FCtable_for.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main() 
-{ 
+{
+    float fahr;
+
+    printf("%s\n", "Fahrenheit to Celcius table");
+    for (fahr = 0; fahr <= 300; fahr = fahr + 20) 
+        printf("%3.0f\t%5.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
 
     return 0;
 }
