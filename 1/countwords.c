009ee893226a3dda22d5abe5cfaebaa86e168e2d
stf <sstefanofm@gmail.com>
2022-08-30 00:39:29 -0300

    count words, lines, and chars

---
+++ 1/countwords.c
@@ -0,0 +1,34 @@
+/* counts words, lines, and characters */
+
+#include <stdio.h>
+
+#define IN  1
+#define OUT 0
+
+int main(void)
+{
+  int c, nl, nw, nc, state;
+
+  state = OUT;
+  nl = nw = nc = 0;
+
+  while ((c = getchar()) != EOF)
+  {
+    ++nc;
+    if (c == '\n')
+      ++nl;
+    if (c == ' ' || c == '\n' || c == '\t')
+      state = OUT;
+    else if (state == OUT)
+    {
+      state = IN;
+      ++nw;
+    }
+  }
+
+  printf("Number of lines: %d\n", nl);
+  printf("Number of words: %d\n", nw);
+  printf("Number of chars: %d\n", nc);
+
+  return 0;
+}
