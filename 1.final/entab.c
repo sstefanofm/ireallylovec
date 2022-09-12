464f01c4d477f49d9c4524c30d874a1b4f807075
stf <sstefanofm@gmail.com>
2022-09-12 17:39:06 -0300

    replacing characters and tabs with the min ammount of blanks

---
+++ 1.final/entab.c
@@ -0,0 +1,80 @@
+/* this program replaces tabs and characters from stdin
+ *  with the minumum ammount of tabs and spaces */
+
+#include <stdio.h>
+
+#define MAXCHAR 200
+#define TABSIZE 8
+
+/* reads a line and returns its length */
+int rcline(char line[], int maxlen)
+{
+    int c, i;
+
+    for (i = 0; i < maxlen - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        line[i] = c;
+    if (c == '\n')
+    {
+        line[i] = '\n';
+        ++i;
+    }
+    line[i] = '\0';
+
+    return i;
+}
+
+/* prints the number of blank characters */
+void printnb(char line[])
+{
+    int nt, ns;
+
+    nt = ns = 0;
+    for (int i = 0; line[i] != '\n' && line[i] != '\0'; ++i)
+        if (line[i] == '\t')
+            ++nt;
+        else
+            ++ns;
+    printf("[\\t:%d,s:%d]\n", nt, ns);
+}
+
+/* creates the 'compressed' line, with the less amount of tabs and spaces */
+void trline(char line[], char trline[], int tabsize)
+{
+    int j, pos;
+
+    j = 0;
+    for (int i = 0; line[i] != '\n' && line[i] != '\0'; ++i)
+    {
+        ++pos;
+        if (line[i] == '\t' || pos >= tabsize)
+        {
+            trline[j] = '\t';
+            ++j;
+            pos = 0;
+        }
+    }
+    for (int i = 0; i < pos; ++i)
+    {
+        trline[j] = ' ';
+        ++j;
+    }
+    trline[j] = '\n';
+    ++j;
+    trline[j] = '\0';
+}
+
+int main(void)
+{
+    char line[MAXCHAR];
+    char newline[MAXCHAR];
+
+    while (rcline(line, MAXCHAR) > 0)
+    {
+        printnb(line);
+        trline(line, newline, TABSIZE);
+        printf("%s", newline);
+        printnb(newline);
+    }
+
+    return 0;
+}
